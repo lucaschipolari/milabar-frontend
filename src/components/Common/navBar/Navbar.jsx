d220751846f98cd407ccd5d6de4423f9ec667cdc
lucas <zlucaschipolariz@gmail.com>
2024-08-12 08:13:31 -0300

    realice el navbar y su estetica

--- src/components/Common/navBar/Navbar.jsx
+++ src/components/Common/navBar/Navbar.jsx
@@ -7,50 +7,50 @@ import {
   faHeart,
 } from "@fortawesome/free-solid-svg-icons";
 
+import "./style.css";
+import { useState } from "react";
+
 const Navbar = () => {
+  const [activeIndex, setActiveIndex] = useState(0);
+  const [cartCount, setCartCount] = useState(0); // Estado para el contador del carrito
+
+  const navItems = [
+    { icon: faHouse, text: "Home", href: "#" },
+    { icon: faAddressBook, text: "Contacto", href: "#" },
+    { icon: faCartShopping, text: "Carrito", href: "#" },
+    { icon: faUsersLine, text: "Nosotros", href: "#" },
+    { icon: faHeart, text: "Favoritos", href: "#" },
+  ];
+
+  // Función para incrementar el contador del carrito (simulación)
+  const incrementCartCount = () => {
+    setCartCount((prevCount) => prevCount + 1);
+  };
+
   return (
-    <div>
+    <div className="div-body">
       <ul className="navigation">
-        <li className="list">
-          <a href="/">
-            <span className="icon"></span>
-            <span className="text">
-              <FontAwesomeIcon icon={faHouse} />
-            </span>
-          </a>
-        </li>
-        <li className="list">
-          <a href="/">
-            <span className="icon"></span>
-            <span className="text">
-              <FontAwesomeIcon icon={faAddressBook} />
-            </span>
-          </a>
-        </li>
-        <li className="list">
-          <a href="/">
-            <span className="icon"></span>
-            <span className="text">
-              <FontAwesomeIcon icon={faCartShopping} />
-            </span>
-          </a>
-        </li>
-        <li className="list">
-          <a href="/">
-            <span className="icon"></span>
-            <span className="text">
-              <FontAwesomeIcon icon={faUsersLine} />
-            </span>
-          </a>
-        </li>
-        <li className="list">
-          <a href="/">
-            <span className="icon"></span>
-            <span className="text">
-              <FontAwesomeIcon icon={faHeart} />
-            </span>
-          </a>
-        </li>
+        {navItems.map((item, index) => (
+          <li
+            key={index}
+            className={`list ${index === activeIndex ? "active" : ""}`}
+            onClick={() => {
+              setActiveIndex(index);
+              if (index === 2) incrementCartCount(); // Incrementa el contador al hacer clic en el carrito
+            }}
+          >
+            <a href={item.href}>
+              <span className="icon-container">
+                <FontAwesomeIcon icon={item.icon} className="icon" />
+                {index === 2 && cartCount > 0 && (
+                  <span className="cart-count">{cartCount}</span>
+                )}
+              </span>
+              <span className="text">{item.text}</span>
+            </a>
+          </li>
+        ))}
+        <div className="indicator"></div>
       </ul>
     </div>
   );
