0651e1886d9907a2ae6d665ae640afaa264b9341
Hernan Alarcon Utz <hernanalarcon51@gmail.com>
2024-08-14 15:41:22 -0300

    Se añadio el mapa a la pagina de contacto

--- src/views/ContactView.jsx
+++ src/views/ContactView.jsx
@@ -1,14 +1,16 @@
-import ContactForm from "../components/ContactView/ContactForm"
-import Map from "../components/ContactView/Map"
-import Logo from "../components/ui/Logo/Logo"
+import ContactForm from "../components/ContactView/ContactForm";
+import Map from "../components/ContactView/Map";
+import Logo from "../components/ui/Logo/Logo";
 
 const ContactView = () => {
   return (
-    <>
-    <Logo />
-    <ContactForm/>
-    <Map />
-    </>
-  )
-}
-export default ContactView
+    <div className="container d-flex flex-column align-items-center text-center px-3">
+      <Logo />
+      <div className="justify-content-center w-100">
+        <ContactForm />
+      </div>
+      <Map />
+    </div>
+  );
+};
+export default ContactView;
