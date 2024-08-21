71fa161a9b462778c31cbe3d5ed2776bf16460c3
lucas <zlucaschipolariz@gmail.com>
2024-08-21 12:22:06 -0300

    realice algunas modificaciones navbar para que aparezca o no el indicator

--- src/views/routing/RootView.jsx
+++ src/views/routing/RootView.jsx
@@ -1,14 +1,20 @@
 import { Outlet, useLocation } from "react-router-dom";
 import Navbar from "../../components/Common/navBar/navbar";
+import HeaderUsuario from "../../components/Common/Header/HeaderUsuario";
 
 const RootView = () => {
   const location = useLocation();
 
   // Mostrar el Header solo si no estamos en la página principal
   const showHeader = location.pathname !== "/";
-
+  const user = {
+    name: "John Doe",
+    avatar: "https://via.placeholder.com/150",
+    isAdmin: true,
+  };
   return (
     <>
+      <HeaderUsuario user={user} />
       <main className="container my-5">
         <Outlet />
       </main>
