a130679a420dbe823bfe6060f6e522c961a3fb17
Hernan Alarcon Utz <hernanalarcon51@gmail.com>
2024-08-23 17:32:55 -0300

    Se modifico el componente del modal del carrito para que tambien pida el numero de mesa antes de realizar el pedido, ademas de agregar el numero de mesa dentro de la funcion que envia los datos al back

--- src/components/CartModal/CartModal.jsx
+++ src/components/CartModal/CartModal.jsx
@@ -2,6 +2,7 @@ import { Dialog } from "primereact/dialog";
 import { Button } from "primereact/button";
 import { useCartStore, useTotalPrice } from "../../stores/useCartStore";
 import { sendProduct } from "../../api/shoppingCart";
+import Input from "../ui/input/Input";
 
 import "./cartModal.css";
 
@@ -12,6 +13,7 @@ import {
   faPlus,
   faShoppingCart,
 } from "@fortawesome/free-solid-svg-icons";
+import { useForm } from "react-hook-form";
 
 const CartModal = ({ visible, onHide }) => {
   const products = useCartStore((state) => state.products);
@@ -19,12 +21,20 @@ const CartModal = ({ visible, onHide }) => {
   const clearCart = useCartStore((state) => state.clearCart);
   const totalPrice = useTotalPrice();
 
-  console.log(products);
+  const {
+    register,
+    formState: { errors },
+    watch,
+  } = useForm();
+
+  const tableNumber = watch("tableNumber", "");
+
   const handleOrder = async () => {
     try {
       const orderDetails = {
         products: products.filter((product) => product.quantity > 0),
         total: totalPrice,
+        tableNumber: tableNumber,
       };
       console.log("Detalles de productos:", orderDetails.products);
       await sendProduct(orderDetails);
@@ -36,17 +46,11 @@ const CartModal = ({ visible, onHide }) => {
     }
   };
 
-  // const handleOrder = () => {
-  //     // Solo para depuración, muestra los productos en la consola
-  //     console.log("Productos en el carrito:", products.filter((product) => product.quantity > 0));
-  //     console.log("Precio total:", totalPrice);
-
-  //     // Elimina el envío al backend por ahora
-  //     // await sendProduct(orderDetails);
-
-  //     clearCart();
-  //     onHide();
-  //   };
+  const isOrderButtonDisabled =
+    !tableNumber ||
+    parseInt(tableNumber) === 0 ||
+    parseInt(tableNumber) > 20 ||
+    !!errors.tableNumber;
 
   return (
     <Dialog
@@ -71,7 +75,7 @@ const CartModal = ({ visible, onHide }) => {
                 className="cart-product dflex align-item-center m-3"
               >
                 <img
-                  src={product.image}
+                  src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ6_VVHpq1t0kCF1wc6SreFBvWGKEYXAayevA&s"
                   alt={product.name}
                   className="product-image"
                 />
@@ -101,14 +105,45 @@ const CartModal = ({ visible, onHide }) => {
             )
         )}
       </div>
-      <div className="cart-total d-flex justify-content-between m-4 price-text">
+      <div className="cart-total d-flex justify-content-between m-3 price-text">
         <h4>Total:</h4>${totalPrice.toFixed(2)}
+      </div>
+      <div className="d-flex align-items-center m-3 table-number-container">
+        <p className="m-0">Ingrese su numero de mesa para hacer el pedido:</p>
+        <Input
+          className="m-3 input-table-number"
+          error={errors.tableNumber}
+          name="tableNumber"
+          options={{
+            required: {
+              value: true,
+              message: "El número de mesa es requerido para realizar el pedido",
+            },
+            validate: {
+              notZero: (value) =>
+                parseInt(value) > 0 || "El número de mesa no puede ser 0",
+              notGreaterThan20: (value) =>
+                parseInt(value) <= 20 ||
+                "El número de mesa no puede ser mayor a 20",
+            },
+          }}
+          placeholder=""
+          register={register}
+          type="number"
+        />
+        {errors.tableNumber && (
+          <p className="error-text">{errors.tableNumber.message}</p>
+        )}
       </div>
       <div className="cart-actions m-3 d-flex justify-content-between">
         <button className="btn btn-danger btn-sm p-1" onClick={clearCart}>
           Vaciar carrito
         </button>
-        <button className="btn btn-success btn-sm p-1" onClick={handleOrder}>
+        <button
+          className="btn btn-success btn-sm p-1"
+          onClick={handleOrder}
+          disabled={isOrderButtonDisabled}
+        >
           Hacer pedido
         </button>
       </div>
