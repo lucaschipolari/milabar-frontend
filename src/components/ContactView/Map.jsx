456af300fcf1b6015a8fe1666496cdbf22b82a0b
Hernan Alarcon Utz <hernanalarcon51@gmail.com>
2024-09-04 14:33:33 -0300

    Se añadieron titulos a la pagina de contacto y tambien el mapa de la sucursal de yerba buena

--- src/components/ContactView/Map.jsx
+++ src/components/ContactView/Map.jsx
@@ -52,7 +52,7 @@ const Map = () => {
             <div className="row">
               <div className="contact-info col-md-6 mt-3">
                 <h3>Sucursal San Miguel de Tucumán</h3>
-                <div className="mt-5">
+                <div className="mt-4">
                   <p>
                     <FontAwesomeIcon
                       icon={faLocationDot}
@@ -101,7 +101,7 @@ const Map = () => {
             <div className="row">
               <div className="contact-info col-md-6 mt-3">
                 <h3>Sucursal Yerba Buena</h3>
-                <div className="mt-5">
+                <div className="mt-4">
                   <p>
                     <FontAwesomeIcon
                       icon={faLocationDot}
@@ -129,7 +129,7 @@ const Map = () => {
 
               <div className="iframe-container col-md-6 mt-3">
                 <iframe
-                  src="https://www.google.com/maps/embed?pb=!1m16!1m12!1m3!1d222.54137355627384!2d-65.20182443446957!3d-26.818889678195!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!2m1!1s25%20de%20Mayo%20899!5e0!3m2!1ses!2sar!4v1723657993046!5m2!1ses!2sar"
+                  src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3560.7302879893036!2d-65.2729417!3d-26.816715299999995!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94225cd9ef2c81e7%3A0x6576d5d1752ac3a9!2sAv.%20Aconquija%20523%2C%20T4107%20Yerba%20Buena%2C%20Tucum%C3%A1n!5e0!3m2!1ses-419!2sar!4v1725470052037!5m2!1ses-419!2sar"
                   className="map-iframe"
                   allowfullscreen
                   loading="lazy"
@@ -140,7 +140,6 @@ const Map = () => {
           </div>
         </div>
       </div>
-
     </div>
   );
 };
--- src/views/ContactView.jsx
+++ src/views/ContactView.jsx
@@ -1,18 +1,21 @@
 import ContactForm from "../components/ContactView/ContactForm";
 import Map from "../components/ContactView/Map";
 
-import Logo from "../components/ui/Logo/Logo";
-
-import "../styles/contactView.css"
+import "../styles/contactView.css";
 
 const ContactView = () => {
   return (
     <div className="bg-contact mb-3">
       <div className="container d-flex flex-column align-items-center text-center px-3">
-        <Logo />
+        <h1 className="my-3 contact-title fw-bold">¡Contactate con nosotros!</h1>
+        <h5 className="m-0 contact-title fw-normal">
+          Por <b>consultas</b> de cualquier tipo o por franquicias llenar este
+          formulario
+        </h5>
         <div className="justify-content-center w-100">
           <ContactForm />
         </div>
+        <h3 className="mb-4 contact-title fw-bold">Nuestras Sucursales</h3>
         <Map />
       </div>
     </div>
