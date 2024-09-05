c32a9340b59a04791b0c4feb5b113d423ad4ffbc
lucas <zlucaschipolariz@gmail.com>
2024-09-05 10:30:53 -0300

    hice el diseño para que no aparezan los botones en el superadmin y la logica para la modal

--- src/components/Admin/Users/UserList.jsx
+++ src/components/Admin/Users/UserList.jsx
@@ -1,13 +1,16 @@
 import PropTypes from "prop-types";
 import { useMutation, useQueryClient } from "@tanstack/react-query";
-import { putUserFn } from "../../../api/usersApi";
+import { putRoleUserFn, putUserFn } from "../../../api/usersApi";
 import { toast } from "sonner";
 import UserModalRole from "./UserModalRole";
 import { useState } from "react";
 import ButtonComponent from "./ButtonComponent";
+import { useUser } from "../../../stores/useUser";
 
 const UserList = ({ filteredUsers, filter }) => {
   const [modalState, setModalState] = useState({ isOpen: false, user: null });
+
+  const { userToEdit, setUserToEdit } = useUser();
 
   const queryClient = useQueryClient();
 
@@ -21,17 +24,39 @@ const UserList = ({ filteredUsers, filter }) => {
       toast.error(`Error actualizando usuario: ${e.message}`);
     },
   });
+  const { mutate: putUserRole } = useMutation({
+    mutationFn: putRoleUserFn,
+    onSuccess: () => {
+      toast.success("Rol actualizado correctamente");
+      queryClient.invalidateQueries(["users", filter]);
+      toast.dismiss();
+    },
+    onError: (e) => {
+      toast.error(`Error actualizando rol: ${e.message}`);
+      toast.dismiss();
+    },
+  });
 
   const handleRole = (roles) => {
     const roleNames = roles.map((role) => role.name);
     if (roleNames.includes("superadmin")) return "SAD";
     if (roleNames.includes("admin")) return "Administrador";
-    if (roleNames.includes("manager")) return "Gerente";
+    if (roleNames.includes("moderator")) return "Moderador";
     return "Usuario";
   };
 
-  const handleOpenModal = (user) => setModalState({ isOpen: true, user });
+  const handleOpenModal = (user) => {
+    setUserToEdit(user);
+    setModalState({ isOpen: true, user });
+  };
   const handleCloseModal = () => setModalState({ isOpen: false, user: null });
+
+  const onSave = (data) => {
+    toast.loading("actualizando entrada");
+    const userId = userToEdit.id;
+    putUserRole({ userId, data: data });
+    handleCloseModal();
+  };
 
   if (!filteredUsers || filteredUsers.length === 0) {
     return <div>No hay usuarios encontrados</div>;
@@ -61,7 +86,6 @@ const UserList = ({ filteredUsers, filter }) => {
                   src={user.avatar || "/default-avatar.jpg"}
                   alt={user.username}
                   className="img-thumbnail"
-                  style={{ width: "50px", height: "50px", objectFit: "cover" }}
                 />
               </td>
               <td>{user.username}</td>
@@ -96,7 +120,8 @@ const UserList = ({ filteredUsers, filter }) => {
           isOpen={modalState.isOpen}
           onClose={handleCloseModal}
           user={modalState.user}
-          roles={modalState.user.roles}
+          roles={modalState.user.roles.map((role) => role.name)}
+          onSave={onSave}
         />
       )}
     </div>
