756f06b283a298ee44c7b83f34392c44a24116c9
Hernan Alarcon Utz <hernanalarcon51@gmail.com>
2024-09-06 15:32:48 -0300

    Se le dio altura y ancho al swiper

--- src/components/ContactView/SwiperContact.jsx
+++ src/components/ContactView/SwiperContact.jsx
@@ -2,38 +2,52 @@ import "swiper/css";
 import "swiper/css/effect-fade";
 import "swiper/css/navigation";
 import "swiper/css/pagination";
+import "swiper/css/autoplay";
 
 import "./swiperStyle.css";
 
-import { EffectFade, Navigation, Pagination } from "swiper/modules";
+import { Autoplay, EffectFade, Navigation, Pagination } from "swiper/modules";
 import { Swiper, SwiperSlide } from "swiper/react";
 
-import chambuchito from "../../assets/chambuchito.jpg"
+import chambuchito from "../../assets/chambuchito.jpg";
 
 const SwiperContact = () => {
   return (
     <>
       <Swiper
+        autoplay={{
+          delay: 3000,
+          disableOnInteraction: false,
+        }}
         spaceBetween={30}
         effect={"fade"}
-        Navigation={true}
+        
         pagination={{
           clickable: true,
         }}
-        modules={[EffectFade, Navigation, Pagination]}
+        modules={[EffectFade, Navigation, Pagination, Autoplay]}
         className="customSwiper"
       >
         <SwiperSlide className="customSwiperSlide">
-          <img src={chambuchito} className="customSwiperImage"/>
+          <img src={chambuchito} className="customSwiperImage" />
         </SwiperSlide>
-        <SwiperSlide >
-          <img src="https://swiperjs.com/demos/images/nature-2.jpg" className="customSwiperImage"/>
+        <SwiperSlide>
+          <img
+            src="https://swiperjs.com/demos/images/nature-2.jpg"
+            className="customSwiperImage"
+          />
         </SwiperSlide>
         <SwiperSlide className="customSwiperSlide">
-          <img src="https://swiperjs.com/demos/images/nature-3.jpg" className="customSwiperImage"/>
+          <img
+            src="https://swiperjs.com/demos/images/nature-3.jpg"
+            className="customSwiperImage"
+          />
         </SwiperSlide>
         <SwiperSlide className="customSwiperSlide">
-          <img src="https://swiperjs.com/demos/images/nature-4.jpg" className="customSwiperImage"/>
+          <img
+            src="https://swiperjs.com/demos/images/nature-4.jpg"
+            className="customSwiperImage"
+          />
         </SwiperSlide>
         <div className="customOverlay">
           <h1 className="text-white fs-1 fw-bold">CONTACTO</h1>
