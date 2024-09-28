af9b2337e94193f87f7eedf29467d9f08a3413cb
Hernan Alarcon Utz <hernanalarcon51@gmail.com>
2024-09-28 19:05:28 -0300

    Se añadio la logica para que los productos no sean añadidos al carrito mientras no esten logeados en la pagina de detalle de producto

--- src/components/DetailPage/AddToCartButton.jsx
+++ src/components/DetailPage/AddToCartButton.jsx
@@ -1,12 +1,24 @@
 import PropTypes from "prop-types";
 import { useCartStore } from "../../stores/useCartStore";
 import { toast } from "sonner";
+import { useSession } from "../../stores/useSession";
+import Swal from "sweetalert2";
 
 const AddToCartButton = (props) => {
   const { producto, quantity } = props;
   const { addProduct } = useCartStore();
+  const { isLoggedIn } = useSession();
 
   const addToCart = () => {
+    if (!isLoggedIn) {
+      Swal.fire({
+        icon: "warning",
+        title: "Debes iniciar sesión",
+        text: "Por favor, inicia sesión para añadir productos al carrito",
+        confirmButtonText: "Ok",
+      });
+      return;
+    }
     addProduct({
       id: producto.id,
       name: producto.nombre,
