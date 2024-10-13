7bdcfd60c5bb6cb66da5e73e7206c163240cda4f
Hernan Alarcon Utz <hernanalarcon51@gmail.com>
2024-10-12 21:07:23 -0300

    Se quitaron las notificaciones de swal, ya que desde el componente contactform ya hacemos manejos de ellas

--- src/utilities/sendEmail.js
+++ src/utilities/sendEmail.js
@@ -1,53 +1,33 @@
 import emailjs from "emailjs-com";
 
-import Swal from "sweetalert2";
-
 export const sendEmailToClient = (data) => {
-  return emailjs
-    .send(
-      "service_8zberdz",
-      "template_a0cdhz8",
-      {
-        name: data.name,
-        lastname: data.lastname,
-        email: data.email,
-        issue: data.issue,
-        message: data.message,
-        to_email: data.email,
-      },
-      "oHlowk37N8JMvbYiC"
-    )
-    .then(
-      (response) => {
-        Swal.fire("Success", "Correo enviado con exito", "success");
-      },
-      (error) => {
-        Swal.fire("Error", "Fallo al enviar el correo", "error");
-      }
-    );
+  return emailjs.send(
+    "service_8zberdz",
+    "template_a0cdhz8",
+    {
+      name: data.name,
+      lastname: data.lastname,
+      email: data.email,
+      issue: data.issue,
+      message: data.message,
+      to_email: data.email,
+    },
+    "oHlowk37N8JMvbYiC"
+  );
 };
 
 export const sendEmailToRestaurant = (data) => {
-  return emailjs
-    .send(
-      "service_8zberdz",
-      "template_q01xpm8",
-      {
-        name: data.name,
-        lastname: data.lastname,
-        email: data.email,
-        issue: data.issue,
-        message: data.message,
-        to_email: "lamilabar899@gmail.com",
-      },
-      "oHlowk37N8JMvbYiC"
-    )
-    .then(
-      (response) => {
-        Swal.fire("Success", "Correo enviado con exito", "success");
-      },
-      (error) => {
-        Swal.fire("Error", "Fallo al enviar el correo", "error");
-      }
-    );
+  return emailjs.send(
+    "service_8zberdz",
+    "template_q01xpm8",
+    {
+      name: data.name,
+      lastname: data.lastname,
+      email: data.email,
+      issue: data.issue,
+      message: data.message,
+      to_email: "lamilabar899@gmail.com",
+    },
+    "oHlowk37N8JMvbYiC"
+  );
 };
